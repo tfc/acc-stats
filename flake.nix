9ea0b81472aacf495b3e2b93d0841de7bdbae8d6
Jacek Galowicz <jacek.galowicz@cyberus-technology.de>
2022-04-16 11:58:46 +0200

    Remove obsolete things and dedupe flake

--- flake.nix
+++ flake.nix
@@ -8,48 +8,40 @@
   };
   outputs = { self, nixpkgs, flake-utils, haskellNix }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-    let
-      projectOverlay = final: prev: {
-        # This overlay adds our project to pkgs
-        accStats =
-          final.haskell-nix.project' {
-            src = final.lib.sourceByRegex ./. [
-              "^acc-.*"
-              "cabal.project"
-            ];
-            compiler-nix-name = "ghc8107";
-            # This is used by `nix develop .` to open a shell for use with
-            # `cabal`, `hlint` and `haskell-language-server`
-            shell.tools = {
-              cabal = {};
-              hlint = {};
-              haskell-language-server = {};
+      let
+        projectOverlay = final: prev: {
+          acc-stats =
+            final.haskell-nix.project' {
+              src = final.lib.sourceByRegex ./. [
+                "^acc-.*"
+                "cabal.project"
+              ];
+              compiler-nix-name = "ghc8107";
+              shell.tools = {
+                cabal = { };
+                hlint = { };
+                haskell-language-server = { };
+              };
+              shell.buildInputs = with pkgs; [
+                nixpkgs-fmt
+              ];
             };
-            # Non-Haskell shell tools go here
-            shell.buildInputs = with pkgs; [
-              nixpkgs-fmt
-            ];
-            # This adds `js-unknown-ghcjs-cabal` to the shell.
-            shell.crossPlatforms = p: [p.ghcjs];
-          };
-      };
-      overlays = [ haskellNix.overlay projectOverlay ];
-      pkgs = import nixpkgs {
-        inherit system overlays;
-        inherit (haskellNix) config;
-      };
-      flake = pkgs.accStats.flake {
-        crossPlatforms = p: [p.ghcjs];
-      };
-      windowsPkgs = import nixpkgs {
-        inherit system overlays;
-        inherit (haskellNix) config;
-        crossSystem = pkgs.lib.systems.examples.mingwW64;
-      };
-      windowsFlake = windowsPkgs.accStats.flake { };
-    in pkgs.lib.recursiveUpdate flake {
-      defaultPackage = flake.packages."acc-stats-server:exe:acc-stats-server";
-      packages.acc-stats-client-windows =
-        windowsFlake.packages."acc-stats-client:exe:acc-stats-client";
-    });
+        };
+        overlays = [ haskellNix.overlay projectOverlay ];
+        nixpkgsConfig = {
+          inherit system overlays;
+          inherit (haskellNix) config;
+        };
+        pkgs = import nixpkgs nixpkgsConfig;
+        flake = pkgs.acc-stats.flake { };
+        windowsPkgs = import nixpkgs (nixpkgsConfig // {
+          crossSystem = pkgs.lib.systems.examples.mingwW64;
+        });
+        windowsFlake = windowsPkgs.acc-stats.flake { };
+      in
+      pkgs.lib.recursiveUpdate flake {
+        defaultPackage = flake.packages."acc-stats-server:exe:acc-stats-server";
+        packages.acc-stats-client-windows =
+          windowsFlake.packages."acc-stats-client:exe:acc-stats-client";
+      });
 }
