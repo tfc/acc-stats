309aa23ea373d993f919e4dcd9706c7baffbe249
Jacek Galowicz <jacek@galowicz.de>
2022-06-04 14:40:51 +0200

    stop dropping db on every start

--- acc-stats-server/src/Acc/Stats/Migrations.hs
+++ acc-stats-server/src/Acc/Stats/Migrations.hs
@@ -17,7 +17,7 @@ runTx con = flip run con . Tx.transaction Tx.ReadCommitted Tx.Write
 
 runMigrations :: Connection -> IO ()
 runMigrations con = do
-    _ <- runTx con (Tx.sql "DROP OWNED BY tfc;")
+    --_ <- runTx con (Tx.sql "DROP OWNED BY tfc;")
     runTx con (runMigration $ MigrationInitialization) >>= print
     migrationScripts <- loadMigrationsFromDirectory "migration-scripts"
     forM_ migrationScripts $ \m@(MigrationScript name script) -> do
